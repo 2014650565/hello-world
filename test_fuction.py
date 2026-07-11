e693c2cb42c159f7d5ceab3ac08702b2e8b24803
shallow0613 <2014610565@qq.com>
2026-07-11 21:06:57 +0800

    添加pytest测试

--- test_fuction.py
+++ test_fuction.py
@@ -1,5 +1,5 @@
 from test_github import greet,add
-
+import requests
 
 
 def test_greet():
@@ -9,3 +9,10 @@ def test_greet():
 def test_add():
     assert add(1,2)==3
     assert add(-1,1)==0
+
+
+
+
+def test_health():
+    resp=requests.request(url="http://43.133.227.52/api/health",method="GET")
+    assert resp.json()["code"]==0
