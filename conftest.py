5e50501c0381e220d06cd082f55d07c0a6e226f6
shallow0613 <2014610565@qq.com>
2026-07-14 21:02:19 +0800

    优化测试用例输出格式，添加换行符以提高可读性

--- conftest.py
+++ conftest.py
@@ -2,9 +2,9 @@ import pytest
 import requests
 @pytest.fixture(scope="session",autouse=True)
 def get_headers():
-    print("用例开始")
+    print("用例开始\n")
     yield {"Authorization":f"Bearer {requests.request(url="http://43.133.227.52/api/login",
                            method="post",
                            json={"username":"tester",
                                  "password":"123456"}).json()["token"]}"}
-    print("用例结束")
+    print("\n用例结束")
